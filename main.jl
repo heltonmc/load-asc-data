d25fdc12de521247328cb0fbb33e999418c9e6ae
Michael Helton <57471570+heltonmc@users.noreply.github.com>
2019-12-16 00:13:12 -0500

    updates

--- main.jl
+++ main.jl
@@ -4,22 +4,23 @@ include("DE-infinite-TR.jl")
 IRF = loadData("/Users/michaelhelton/Documents/load-asc-data/IRF_10mm_SDS_750nm.asc")
 DTOF = loadData("/Users/michaelhelton/Documents/load-asc-data/IL5_10mm_SDS_750nm_c1.asc")
 DTOF1 = loadData1("/Users/michaelhelton/Documents/load-asc-data/IL5_10mm_SDS_750nm_20ml.asc")
-
+DTOF2 =loadData1("/Users/michaelhelton/Documents/load-asc-data/IL5_10mm_SDS_750nm_20ml.asc")
 maxval,maxin = findmax(DTOF1.counts)
 
 using LsqFit
-lb = [0.001, 3]
-ub = [0.5, 13]
-w  = Vector(1:1:length(DTOF1.counts)).^4
-β0 = [0.2, 10]
-fit = curve_fit(conv_DT_IRF, IRF.time[1:length(DTOF1.time)], DTOF1.counts./maximum(DTOF1.counts),w,β0,lower = lb,upper = ub)
-
+function fitDTOF(DTOF1)
+    lb = [0.001, 3]
+    ub = [0.5, 50]
+    w  = Vector(1:1:length(DTOF1.counts)).^4
+    β0 = [0.2, 35]
+    fit = curve_fit(conv_DT_IRF, IRF.time[1:length(DTOF1.time)], DTOF1.counts./maximum(DTOF1.counts),w,β0,lower = lb,upper = ub)
+end
 
 xfit = IRF.time[1:length(DTOF1.time)]
 yfit = conv_DT_IRF(xfit,fit.param)
 
-scatter(DTOF1.counts./maximum(DTOF1.counts), color="black",yaxis =:log,label = "Expt",markersize=3,alpha=0.8)
-plot!(yfit, color="red", linewidth=3, label = "DT-fit",alpha = 0.8)
+scatter(xfit,DTOF1.counts./maximum(DTOF1.counts), color="black",yaxis =:log,label = "Expt",markersize=3,alpha=0.8)
+plot!(xfit,yfit, color="red", linewidth=3, label = "DT-fit",alpha = 0.8)
 xlabel!("time (ns)")
 ylabel!("Reflectance [counts]")
 title!("\\mu_{a} = $(round(fit.param[1],digits=4)) cm^{-1}, \\mu_{s}' = $(round(fit.param[2],digits=3)) cm^{-1}")
